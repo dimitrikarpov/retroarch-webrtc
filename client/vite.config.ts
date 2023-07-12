ffd344aaa9553e5ed5759989e3ed8ed8564d239c
dimitrikarpov <karpov.dimon@gmail.com>
2023-07-12 15:07:28 +0500

    initial production docker build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     host: true,
-    port: 3000, // This is the port which we will use in docker
-    // Thanks @sergiomoura for the window fix
-    // add the next lines if you're using windows and hot reload doesn't work
+    strictPort: true,
+    port: 3000,
     watch: {
       usePolling: true,
     },
